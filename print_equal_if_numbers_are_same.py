19eb8c36686bedd4e1aa07c3eed081959b28c504
conradjosesalayog <radradsalayog@gmail.com>
2026-03-09 03:19:43 +0800

    Added else statement and attached formatted strings to the print function that prints otherwise

--- print_equal_if_numbers_are_same.py
+++ print_equal_if_numbers_are_same.py
@@ -2,3 +2,5 @@ num_1 = int(input("Enter first number: "))
 num_2 = int(input("Enter second number: "))
 if num_1 == num_2:
     print("Equal")
+else:
+    print(f"The numbers {num_1} & {num_2} are not equal")
