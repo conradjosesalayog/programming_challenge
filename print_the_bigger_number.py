05be7d8638bafb9d64d03e43d8644f6f6eb42a04
conradjosesalayog <radradsalayog@gmail.com>
2026-03-09 02:57:27 +0800

    Including if and else statements and print num_1 if it is bigger and else, print num_2

--- print_the_bigger_number.py
+++ print_the_bigger_number.py
@@ -1,2 +1,6 @@
 num_1 = int(input("Enter first number: "))
 num_2 = int(input("Enter second number: "))
+if num_1 > num_2:
+    print("The bigger number is", num_1)
+else:
+    print("The bigger number is ", num_2)
