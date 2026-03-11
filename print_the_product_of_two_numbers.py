543f967f2bd734e8c08ac3b799728fe6c1c758d2
conradjosesalayog <radradsalayog@gmail.com>
2026-03-11 13:52:44 +0800

    Added input function for two numbers

--- print_the_product_of_two_numbers.py
+++ print_the_product_of_two_numbers.py
@@ -1,3 +1,3 @@
-num1 = int(input("Please enter the first number: "))
-num2 = int(input("Please enter the second number: "))
-print("The product of the two numbers is: ", num1 * num2)
+num_1 = int(input("Please enter the first number: "))
+num_2 = int(input("Please enter the second number: "))
+
