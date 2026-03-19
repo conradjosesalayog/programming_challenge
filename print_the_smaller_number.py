8e349be61954d0badfd9cdbf7d9c24f2c8562e34
conradjosesalayog <radradsalayog@gmail.com>
2026-03-20 02:53:55 +0800

    Added else statement that prints/displays num_2 if it is smaller.

--- print_the_smaller_number.py
+++ print_the_smaller_number.py
@@ -2,3 +2,5 @@ num_1 = int(input("Enter first number: "))
 num_2 = int(input("Enter second number: "))
 if num_1 < num_2:
     print("The smaller number is", num_1)
+else:
+    print("The smaller number is ", num_2)
