3905916a55e667e5da93653f2e2a02bad332cadd
conradjosesalayog <radradsalayog@gmail.com>
2026-03-20 19:17:42 +0800

    Implement for loop and condition to filter and store non-duplicated numbers into not_duplicated list

--- print_first_entry_numbers_if_duplicated.py
+++ print_first_entry_numbers_if_duplicated.py
@@ -2,3 +2,8 @@ numbers = []
 not_duplicated_numbers = []
 for i in range(10):
     numbers.append(int(input("Enter number: ")))
+
+for num in numbers:
+    if numbers.count(num) == 1:
+       not_duplicated_numbers.append(num)
+print("Not duplicated:", not_duplicated_numbers)
