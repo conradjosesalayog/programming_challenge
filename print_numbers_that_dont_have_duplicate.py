3cd14f7507ab3396a0d6e0a049eb82da020a3056
conradjosesalayog <radradsalayog@gmail.com>
2026-03-20 18:23:26 +0800

    Added for loop to the numbers in the number_list. Also added if statement wherein if the number is inputted only once, then that will be the only numbers displayed

--- print_numbers_that_dont_have_duplicate.py
+++ print_numbers_that_dont_have_duplicate.py
@@ -2,3 +2,7 @@ numbers_list = []
 for i in range(10):
     number = int(input("Enter number: "))
     numbers_list.append(number)
+print("Numbers without duplicate:")
+for number in numbers_list:
+    if numbers_list.count(number) == 1:
+        print(number)
