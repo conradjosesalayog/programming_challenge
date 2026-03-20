8e61bce508fc3b8bf14c4908618d7cb368ea6038
conradjosesalayog <radradsalayog@gmail.com>
2026-03-20 20:36:06 +0800

    Variable num was added in numbers using if statement. If num is in numbers already, it will print Duplicate while if it is not, else statement was used and it will print Unique

--- print_unique_and_duplicate.py
+++ print_unique_and_duplicate.py
@@ -4,3 +4,11 @@ while True:
 
     if not number_input.isdigit():
         break
+
+    num = int(number_input)
+
+    if num in numbers:
+        print("Duplicate")
+    else:
+        print("Unique")
+        numbers.append(num)
