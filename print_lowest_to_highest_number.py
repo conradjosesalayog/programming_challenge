d1b00e108b1aeca179d76b5cefb13b49c7c19698
conradjosesalayog <radradsalayog@gmail.com>
2026-03-20 23:49:44 +0800

    Check if input is a number. If the input is a number (including decimals), add it to the list.

--- print_lowest_to_highest_number.py
+++ print_lowest_to_highest_number.py
@@ -2,3 +2,7 @@ numbers = []
 
 while True:
     user_input = input("Enter number: ")
+
+    if user_input.replace('.', '', 1).isdigit():
+        number_value = float(user_input)
+        numbers.append(number_value)
