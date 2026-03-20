3c9ab6c33fe4093ba81f5994321d47afd215adfe
conradjosesalayog <radradsalayog@gmail.com>
2026-03-20 23:32:04 +0800

    Used try in case of error. Added if statement if number value is less than the lowest number

--- print_the_lowest_number.py
+++ print_the_lowest_number.py
@@ -2,3 +2,8 @@ lowest_number = None
 
 while True:
     user_input = input("Enter number: ")
+
+    try:
+        number_value = int(user_input)
+        if lowest_number is None or number_value < lowest_number:
+            lowest_number = number_value
