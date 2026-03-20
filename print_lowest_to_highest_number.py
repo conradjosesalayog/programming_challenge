525783ac8fa85c549f70daeee3b6b42feffae8a4
conradjosesalayog <radradsalayog@gmail.com>
2026-03-20 23:53:11 +0800

    Added else statement. If it’s invalid (letters, empty, etc.), stop

--- print_lowest_to_highest_number.py
+++ print_lowest_to_highest_number.py
@@ -6,3 +6,9 @@ while True:
     if user_input.replace('.', '', 1).isdigit():
         number_value = float(user_input)
         numbers.append(number_value)
+    else:
+        break
+
+numbers.sort()
+
+print("Numbers from lowest to highest:", numbers)
