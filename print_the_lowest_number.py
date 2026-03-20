8850d76c3e988e5b8ad0175f473cef25227c0079
conradjosesalayog <radradsalayog@gmail.com>
2026-03-20 23:33:45 +0800

    Used except to break the program if it crashes. Added print function for the lowest number.

--- print_the_lowest_number.py
+++ print_the_lowest_number.py
@@ -7,3 +7,7 @@ while True:
         number_value = int(user_input)
         if lowest_number is None or number_value < lowest_number:
             lowest_number = number_value
+    except:
+        break
+
+print("Lowest:", lowest_number)
