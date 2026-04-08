ba247b1e5fbbc38b88cc4e9a7729d928811eb1cc
conradjosesalayog <radradsalayog@gmail.com>
2026-04-08 17:03:08 +0800

    Added lists for the highest count and the most duplicated to None and 0. Added for loop wherein if count is greater than the highest count, it will be the same value as well as for most_duplicate and num. Added display function

--- display_highest_number.py
+++ display_highest_number.py
@@ -6,3 +6,14 @@ while True:
         numbers.append(num)
     except:
         break
+
+most_duplicate = None
+highest_count = 0
+
+for num in numbers:
+    count = numbers.count(num)
+    if count > highest_count:
+        highest_count = count
+        most_duplicate = num
+
+print("Number with most duplicates:", most_duplicate)
