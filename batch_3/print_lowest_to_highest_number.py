375aa0ad3874c73087c070ac799661688c9a091a
conradjosesalayog <radradsalayog@gmail.com>
2026-04-09 01:53:58 +0800

    Made the sequence more simpler

--- batch_3/print_lowest_to_highest_number.py
+++ batch_3/print_lowest_to_highest_number.py
@@ -1,14 +1,9 @@
 numbers = []
-
 while True:
-    user_input = input("Enter number: ")
-
-    if user_input.replace('.', '', 1).isdigit():
-        number_value = float(user_input)
-        numbers.append(number_value)
-    else:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except:
         break
-
 numbers.sort()
-
-print("Numbers from lowest to highest:", numbers)
+print("Highest to lowest:" , numbers)
