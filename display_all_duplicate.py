6d7fb88bb8e3a7a0353b259019ce1eb5ed772674
conradjosesalayog <radradsalayog@gmail.com>
2026-04-08 16:25:08 +0800

    List for numbers and set the range to 10 as well as added input function and append function

--- display_all_duplicate.py
+++ display_all_duplicate.py
@@ -4,8 +4,3 @@ for i in range(10):
     num = int(input("Enter a number: "))
     numbers.append(num)
 
-duplicates = []
-for num in numbers:
-    if numbers.count(num) > 1 and num not in duplicates:
-        duplicates.append(num)
-print("Numbers that has duplicates: ", duplicates)
