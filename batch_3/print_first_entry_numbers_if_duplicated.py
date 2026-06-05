b4787df7fb29446a41b6f1cdeb9376a2e9102fcd
Conrad Jose Salayog <radradsalayog@gmail.com>
2026-06-06 01:16:24 +0800

    updated the program. made it simpler

--- batch_3/print_first_entry_numbers_if_duplicated.py
+++ batch_3/print_first_entry_numbers_if_duplicated.py
@@ -1,9 +1,6 @@
-numbers = []
-not_duplicated_numbers = []
+number_list = []
 for i in range(10):
-    numbers.append(int(input("Enter number: ")))
-
-for num in numbers:
-    if numbers.count(num) == 1:
-       not_duplicated_numbers.append(num)
-print("Not duplicated:", not_duplicated_numbers)
+    num = int(input("Enter a number:"))
+    if num not in number_list:
+        number_list.append(num)
+print(number_list)
